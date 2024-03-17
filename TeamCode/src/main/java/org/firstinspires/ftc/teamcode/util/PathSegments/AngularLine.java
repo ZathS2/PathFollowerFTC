1672cfdbd66efe4cf8d0c9918b5964ea24505363
ZathS2 <mateusmrm.mrm@gmail.com>
2024-03-16 22:47:54 -0300

    Implementa busca binária na busca da tabela
    Além disso, começa a preparar a lidar com ângulo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PathSegments/AngularLine.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.util.PathSegments;
+
+import org.firstinspires.ftc.teamcode.util.geometry.Pose2d;
+import org.firstinspires.ftc.teamcode.util.geometry.Vector2d;
+
+public class AngularPath
+{
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/interfaces/AngularPathSegment.java
@@ -0,0 +1,2 @@
+package org.firstinspires.ftc.teamcode.util.interfaces;public interface AngularPathSegment {
+}
